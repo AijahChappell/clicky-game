59e55ff890cce5306ec80e434f4975ebd9551303
Aijah Chappell <aijahc@comcast.net>
2019-09-22 15:43:08 -0400

    defining default values for state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    currentScore: 0,
+    topScore: 0,
+    message: "Click any image to start the game",
+    id1 :0,
+    id2 :0,
+    id3 :0,
+    id4 :0,
+    id5 :0,
+    id6 :0,
+    id7 :0,
+    id8 :0,
+    id9 :0,
+    id10 :0,
+    id11 :0,
+    id12 :0
+  };
+
 }
 
 export default App;
