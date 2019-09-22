05992997f28a75166f25b2422bbd727e36d2f4de
Aijah Chappell <aijahc@comcast.net>
2019-09-22 16:07:30 -0400

    Header component with memory game image

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="header">
+            <img src="https://i.imgur.com/tBRoT3g.jpg" alt="Memory Game"></img>
+        </div>
+    )
+}
+
+export default Header;
