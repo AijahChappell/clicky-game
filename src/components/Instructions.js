0f4eae648620929d66d8e716f470f17205a84556
Aijah Chappell <aijahc@comcast.net>
2019-09-22 16:11:27 -0400

    Instructions component

---
+++ src/components/Instructions.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Instructions () {
+    return (
+        <div className="instructions">
+            <p>Instructions: Click an image to earn points. Game is over if you click the same image twice!</p>
+        </div>
+    )
+}
+
+export default Instructions;
